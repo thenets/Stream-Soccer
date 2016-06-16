34199e14f8b22e7b16de68b066e7433132669949
Luiz Costa <phelip@thenets.org>
2016-06-16 10:21:57 -0300

    NPM packages added to /public

--- application/modules/layout/views/footer.php
+++ application/modules/layout/views/footer.php
@@ -1,15 +1,7 @@
-
-
-    <!--
-    <script src="https://cdn.lfrs.sl/alloyui.com/3.0.1/aui/aui-min.js"></script>
-    -->
-
-    <!-- JS -->
-    <link rel="stylesheet" type="text/css" class="ui" href="//oss.maxcdn.com/semantic-ui/2.1.8/components/component-name.min.css">
-	<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.5/angular.js"></script>
-	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    <?php echo SynCompileJS() ?>
+    <div class="container">
+        <hr>
+        <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'SynIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+    </div>
 
   </body>
 </html>
--- application/modules/layout/views/header.php
+++ application/modules/layout/views/header.php
@@ -5,14 +5,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Stream Soccer / UNIFEI</title>
+    <title>Stream Soccer | UNIFEI</title>
 
     <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/flatly/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
     
     <!-- SynCompiler : CSS -->
     <?php echo SynCompileCSS() ?>
-    
+
+    <!-- SynCompiler : JS -->
+    <script src="<?=base_url('public/node_modules/core-js/client/shim.min.js')?>"></script>
+    <script src="<?=base_url('public/node_modules/zone.js/dist/zone.js')?>"></script>
+    <script src="<?=base_url('public/node_modules/reflect-metadata/Reflect.js')?>"></script>
+    <script src="<?=base_url('public/node_modules/rxjs/bundles/Rx.umd.js')?>"></script>
+    <script src="<?=base_url('public/node_modules/@angular/core/core.umd.js')?>"></script>
+    <script src="<?=base_url('public/node_modules/@angular/common/common.umd.js')?>"></script>
+    <script src="<?=base_url('public/node_modules/@angular/compiler/compiler.umd.js')?>"></script>
+    <script src="<?=base_url('public/node_modules/@angular/platform-browser/platform-browser.umd.js')?>"></script>
+    <script src="<?=base_url('public/node_modules/@angular/platform-browser-dynamic/platform-browser-dynamic.umd.js')?>"></script>
+
+    <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <?php echo SynCompileJS() ?>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -22,3 +36,30 @@
     <![endif]-->
   </head>
   <body>
+
+<nav class="navbar navbar-default navbar-static-top">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="<?=base_url()?>">Stream Soccer</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a href="<?php echo base_url() ?>">Buscar partidas</a></li>
+        <li><a href="<?php echo base_url() ?>">Campeonatos</a></li>
+      </ul>
+
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="<?php echo base_url('admin') ?>">Admin</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
